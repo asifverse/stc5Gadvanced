278ec616c3f605d609aadb7ce6001d6047904403
asifverse <asifverse.dev@gmail.com>
2025-06-29 17:49:15 +0300

    Add files via upload

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,22 +1,71 @@
-        $(document).ready(function () {
-            $(".cs-carousel").owlCarousel({
-                loop: true,
-                margin: 20,
-                nav: false,
-                dots: true,
-                responsive: {
-                    0: {
-                        items: 1
-                    },
-                    576: {
-                        items: 2
-                    },
-                    768: {
-                        items: 3
-                    },
-                    992: {
-                        items: 4
-                    }
-                }
-            });
-        });
+$(document).ready(function () {
+    $(".cs-carousel").owlCarousel({
+        loop: false,
+        margin: 20,
+        nav: false,
+        dots: true,
+        responsive: {
+            0: {
+                items: 2
+            },
+            576: {
+                items: 2
+            },
+            768: {
+                items: 2
+            },
+            992: {
+                items: 4
+            }
+        }
+    });
+});
+
+
+
+$(document).ready(function () {
+    $(".device-cards-container").owlCarousel({
+        loop: false,
+        margin: 20,
+        nav: false,
+        dots: true,
+        responsive: {
+            0: {
+                items: 2
+            },
+            576: {
+                items: 2
+            },
+            768: {
+                items: 2
+            },
+            992: {
+                items: 4
+            }
+        }
+    });
+});
+
+
+$(document).ready(function () {
+    $(".device-cards-container-2").owlCarousel({
+        loop: false,
+        margin: 20,
+        nav: false,
+        dots: true,
+        responsive: {
+            0: {
+                items: 2
+            },
+            576: {
+                items: 2
+            },
+            768: {
+                items: 2
+            },
+            992: {
+                items: 4
+            }
+        }
+    });
+});
